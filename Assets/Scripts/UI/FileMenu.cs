5f10a89737e361ac1714f861fbccc6ae6c72762a
Jonathan Chen <jonathanchen@gatech.edu>
2017-06-18 20:41:20 -0400

    Level names

--- Assets/Scripts/UI/FileMenu.cs
+++ Assets/Scripts/UI/FileMenu.cs
@@ -11,47 +11,55 @@ namespace Assets.Scripts.UI
         [SerializeField]
         private DialogueMenu dialogueMenu;
         [SerializeField]
-        private Text loadLevelTextField;
+        private OptionsMenu optionsMenu;
 
-        // TODO Temporary
-        // Note: Lowercase file names only
-        private static readonly string filePath = @"/level.csv";
+        [SerializeField]
+        private Text loadLevelText;
 
         public void OnRun()
         {
-            dialogueMenu.CloseDialogue();
+
         }
 
         public void OnSave()
         {
-            File.WriteAllText(Application.dataPath + filePath, GridManager.Instance.FormatToCSV());
+            string fileName = optionsMenu.LevelName + ".csv";
+            File.WriteAllText(Application.dataPath + "/" + fileName, GridManager.Instance.FormatToCSV());
         }
 
         public void OnLoad()
         {
-            dialogueMenu.CloseDialogue();
-            Debug.Log(loadLevelTextField.text.ToLower());
+            string newLevelName = loadLevelText.text.ToLower().Replace(' ', '_');
+            string filePath = Application.dataPath + "/" + newLevelName + ".csv";
 
-            // TODO Check file exists
-            string[] lines = File.ReadAllLines(Application.dataPath + filePath);
-            string[] gridSize = lines[0].Split(',');
-            GridManager.Instance.SetGridSize(int.Parse(gridSize[0]), int.Parse(gridSize[1]));
-            for(int i = 1; i < lines.Length; i++)
+            if(File.Exists(filePath))
             {
-                string[] line = lines[i].Split(',');
-                GridManager.Instance.AddGridObject(SpriteManager.Instance.GetSprite(line[0]), int.Parse(line[1]), int.Parse(line[2]));
+                optionsMenu.SetLevelName(newLevelName);
+                
+                // Parse file
+                string[] lines = File.ReadAllLines(filePath);
+                string[] gridSize = lines[0].Split(',');
+                GridManager.Instance.SetGridSize(int.Parse(gridSize[0]), int.Parse(gridSize[1]));
+                for(int i = 1; i < lines.Length; i++)
+                {
+                    string[] line = lines[i].Split(',');
+                    GridManager.Instance.AddGridObject(SpriteManager.Instance.GetSprite(line[0]), int.Parse(line[1]), int.Parse(line[2]));
+                }
+            }
+            else
+            {
+                dialogueMenu.OpenDialogue(Dialogue.LoadLevel);
             }
         }
 
         public void OnClear()
         {
-            dialogueMenu.CloseDialogue();
             GridManager.Instance.ClearGrid();
         }
 
         public void OnOptions()
         {
-            dialogueMenu.CloseDialogue();
+
         }
 
         public void OnExit()
---
+++ Assets/Scripts/UI/OptionsMenu.cs
@@ -0,0 +1,63 @@
+﻿using Assets.Scripts.Core;
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Assets.Scripts.UI
+{
+    public class OptionsMenu : MonoBehaviour
+    {
+        public bool GridView { get; private set; }
+        public bool HoverScroll { get; private set; }
+        public string LevelName { get; private set; }
+
+        [SerializeField]
+        private Image gridViewIcon;
+        [SerializeField]
+        private Image hoverScrollIcon;
+        [SerializeField]
+        private Text levelNameText;
+
+        [SerializeField]
+        private Sprite iconCheck;
+        [SerializeField]
+        private Sprite iconCross;
+
+        public void Awake()
+        {
+            // Initialize options to defaults
+            GridView = true;
+            gridViewIcon.sprite = iconCheck;
+
+            HoverScroll = true;
+            hoverScrollIcon.sprite = iconCheck;
+
+            SetLevelName("level");
+        }
+
+        public void ToggleGridView()
+        {
+            // TODO
+            GridView = !GridView;
+            gridViewIcon.sprite = GridView ? iconCheck : iconCross;
+        }
+
+        public void ToggleHoverScroll()
+        {
+            // TODO
+            HoverScroll = !HoverScroll;
+            hoverScrollIcon.sprite = HoverScroll ? iconCheck : iconCross;
+        }
+
+        public void OnLevelName()
+        {
+            SetLevelName(levelNameText.text);
+        }
+
+        public void SetLevelName(string levelName)
+        {
+            LevelName = levelName.ToLower().Replace(' ', '_');
+            levelNameText.text = LevelName;
+        }
+    }
+}
