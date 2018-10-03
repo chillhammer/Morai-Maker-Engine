e8249989ed27f6481bca3eea34d190c1ad8a2e07
Andrew Hoyt <andrew@chillhammer.com>
2018-10-03 12:37:36 -0400

    Branching From First Node

--- Assets/PlayabilityCheck/PathFinding/PathFinding.cs
+++ Assets/PlayabilityCheck/PathFinding/PathFinding.cs
@@ -2,6 +2,8 @@
 using System.Collections;
 using System.Collections.Generic;
 
+using Assets.Scripts.Core;
+
 namespace Assets.PlayabilityCheck.PathFinding
 {
 	public class PathFinding
@@ -10,8 +12,32 @@ namespace Assets.PlayabilityCheck.PathFinding
 		public int characterHeight = 1;
 		public int characterJumpHeight = 5;
 
-		public List<Vector2> FindPath(Vector2 start, Vector2 end)
-		{
+
+		private List<PFNode>[] nodes; //Grid of List of Nodes
+		private Stack<int> traversedCoordinates; //(x + y*width)
+
+
+
+		public List<Vector2> FindPath(Vector2 start, Vector2 end) {
+
+			//Convert From Vector to Location
+			Location myLocation = new Location((int)start.y * GridManager.Instance.GridWidth + (int)start.x, 0);
+			Location endLocation = new Location((int)end.y * GridManager.Instance.GridHeight + (int)end.x, 0);
+
+			//First Node To Branch From
+			PFNode firstNode = new PFNode();
+			firstNode.G = 0;
+			firstNode.F = 1;
+			firstNode.PX = (ushort)start.x;
+			firstNode.PY = (ushort)start.y;
+			firstNode.PZ = 0;
+			firstNode.Status = NodeValue.Open;
+
+			//Setting Jump Length
+			if (GridManager.Instance.ContainsGridObject(true, (int)start.x, (int)start.y - 1))
+				firstNode.JumpLength = 0;
+			else
+				firstNode.JumpLength = (short)(characterJumpHeight * 2);
 
 			return new List<Vector2>();
 		}
@@ -32,6 +58,11 @@ namespace Assets.PlayabilityCheck.PathFinding
 			public int z;
 		}
 
+		public enum NodeValue
+		{
+			Open
+		}
+
 		internal struct PFNode
 		{
 			public int F; // F = gone + heuristic
@@ -41,6 +72,9 @@ namespace Assets.PlayabilityCheck.PathFinding
 			public int PX;
 			public int PY;
 			public int PZ;
+
+			public object Status { get; internal set; }
+			public int JumpLength { get; internal set; }
 		}
 		#endregion
 
